3d90f579a22774adf2e24077ce3f6b125f27fd7a
Krytishok <zhigalinleonid@gmail.com>
2024-10-14 22:30:48 +1000

    Building a customization of car

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -9,6 +9,11 @@ public class MainMenuScript : MonoBehaviour
     [SerializeField] GameObject _buttonPlay;
     [SerializeField] GameObject _buttonExit;
     [SerializeField] GameObject _buttonToGarage;
+    [SerializeField] GameObject _buttonMuscle;
+    [SerializeField] GameObject _buttonHatchback;
+
+    [SerializeField] GameObject _carCollection;
+
 
     [SerializeField] GameObject _mainCamera;
 
@@ -29,6 +34,9 @@ public class MainMenuScript : MonoBehaviour
         Time.timeScale = 1.0f;
         _NumberOfCoinsText.text = PlayerPrefs.GetInt("Coins").ToString();
 
+        _buttonHatchback.SetActive(false);
+        _buttonMuscle.SetActive(false);
+
     }
     public void PlayGame()
     {
@@ -47,6 +55,21 @@ public class MainMenuScript : MonoBehaviour
         _buttonPlay.SetActive(false);
         _buttonToGarage.SetActive(false);
 
+        _buttonMuscle.SetActive(true);
+        _buttonHatchback.SetActive(true);
 
+
+    }
+    public void ChooseHatch()
+    {
+        _carCollection.GetComponentAtIndex(0).gameObject.SetActive(true);
+        _carCollection.GetComponentAtIndex(1).gameObject.SetActive(false);
+        PlayerPrefs.SetString("Vehicle", "Hatchback");
+    }
+    public void ChooseMuscle()
+    {
+        _carCollection.GetComponentAtIndex(0).gameObject.SetActive(false);
+        _carCollection.GetComponentAtIndex(1).gameObject.SetActive(true);
+        PlayerPrefs.SetString("Vehicle", "Muscle");
     }
 }
