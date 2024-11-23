05622b10f98fcb787960b2d22c9308ebcaf54d32
Krytishok <zhigalinleonid@gmail.com>
2024-11-23 15:38:53 +1000

    Particles from wheels

---
+++ Assets/Scripts/CarEffectController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class CarEffectController : MonoBehaviour
+{
+    [SerializeField] private float _valueToStartParticles;
+
+    [SerializeField] private ParticleSystem _particleSystemLeft;
+    [SerializeField] private ParticleSystem _particleSystemRight;
+
+    [SerializeField] private TrailRenderer _trailRendererLeft;
+    [SerializeField] private TrailRenderer _trailRendererRight;
+
+    private float _particleRate;
+
+    private void Start()
+    {
+
+    }
+
+
+    public void UpdateEffect(float move)
+    {
+        if(Mathf.Abs(move) == _valueToStartParticles)
+        {
+            _particleSystemLeft.Play();
+            _particleSystemRight.Play();
+
+
+        } else
+        {
+            _particleSystemLeft.Stop();
+            _particleSystemRight.Stop();
+        }
+    }
+}
