1c95fa3e0355923bef8339947211450072c2e20a
sinetskiy05 <sinetskiy05@mail.ru>
2024-12-13 16:25:10 +1000

    addDrill

---
+++ Assets/Scripts/DrillTrigger.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class DrillTrigger : MonoBehaviour
+{
+    private Animator animator; // Ссылка на Animator родительского объекта
+    private bool Active = false; // Флаг для контроля генерации
+    //private CameraController camControll;
+    public ParticleSystem partSystem;
+
+    private void Start()
+    {
+        // Ищем Animator на родительском объекте
+        animator = GetComponentInParent<Animator>();
+        //camControll = FindFirstObjectByType<CameraController>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Проверяем, что объект с тегом "Player" входит в триггер
+        if (other.CompareTag("Player") && !(Active))
+        {
+            // Запускаем анимацию, если Animator найден
+            if (animator != null)
+            {
+                partSystem.Play();
+                animator.SetBool("Drill_Animation", true); // Укажите имя анимации
+                //camControll.CameraShake();
+            }
+            Active = true;
+        }
+    }
+}
